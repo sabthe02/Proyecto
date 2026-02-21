6a6153700b7745f5c222ef9c991075f09f0cec95
Juanse <juan.beron2001@gmail.com>
2026-02-20 23:34:14 -0300

    Se agrega logica de movimiento WebSocket

--- Frontend/src/scenes/Start.js
+++ Frontend/src/scenes/Start.js
@@ -11,10 +11,47 @@ export class Start extends Phaser.Scene {
 
         this.dot = this.add.circle(400, 300, 10, 0x0fffff);
 
+        this.loadPosition();
+
+        this.socket = new WebSocket("ws://localhost:8080/ws");
+
+        this.socket.onopen = () => {
+            console.log("Connectado a servidor WebSocket");
+            this.socket.send("Hola de Phaser");
+        };
+
+        this.socket.onmessage = (event) => {
+            console.log("Mensaje del servidor:", event.data);
+        };
+
+      //Muevo objeto cuando recibo mensaje del backend  
+      this.socket.onmessage = (event) => {
+
+        const data = JSON.parse(event.data);
+
+        if (data.tipo === "MOVER") {
+            this.dot.x = data.x;
+            this.dot.y = data.y;
+        }
+    };
+
+        // Detecto movimiento del mouse
         this.input.on('pointermove', (pointer) => {
+
             this.dot.x = pointer.x;
             this.dot.y = pointer.y;
-        });
+
+                if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+
+                    const mensaje = {
+                         tipo: "MOVER",
+                         x: pointer.x,
+                         y: pointer.y
+                 };
+
+                this.socket.send(JSON.stringify(mensaje));
+    }
+});
 
         this.add.text(16, 16, 'Mover punto con el mouse', {
             fontSize: '20px',
@@ -79,18 +116,7 @@ export class Start extends Phaser.Scene {
         botonCargar.x = 700;
         botonCargar.y = 80;
 
-        this.loadPosition();
 
-        this.socket = new WebSocket("ws://localhost:8080/ws");
-
-        this.socket.onopen = () => {
-            console.log("Connectado a servidor WebSocket");
-            this.socket.send("Hola de Phaser");
-        };
-
-        this.socket.onmessage = (event) => {
-            console.log("Mensaje del servidor:", event.data);
-        };
 
 
         this.socket.onerror = (error) => {

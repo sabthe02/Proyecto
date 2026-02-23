6a61372b1c9f4aca34d5548cee5274ba0911f6e5
Juanse <juan.beron2001@gmail.com>
2026-02-23 01:30:56 -0300

    Se agrega modulo main para ir puliendo logica de juego

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,13 +2,14 @@ import { Greeting } from './scenes/Greeting.js';
 import { GameChoice } from './scenes/GameChoice.js';
 import { Start } from './scenes/Start.js';
 import { Lobby } from './scenes/Lobby.js';
+import { Game } from './scenes/Game.js';
 
 const config = {
     type: Phaser.AUTO,
     parent: 'game-container',
     width: window.innerWidth,
     height: window.innerHeight,
-    scene: [Greeting, GameChoice, Start, Lobby],
+    scene: [ Greeting, GameChoice, Start, Lobby, Game ],
 
     scale: {
         mode: Phaser.Scale.RESIZE,

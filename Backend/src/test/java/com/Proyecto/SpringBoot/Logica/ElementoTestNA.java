8a5295ae42c29fba87f389a451c9f5abf7b0f071
santi-pereira <spereira258@gmail.com>
2026-03-08 21:28:54 -0300

    ,

--- Backend/src/test/java/com/Proyecto/SpringBoot/Logica/ElementoTestNA.java
+++ Backend/src/test/java/com/Proyecto/SpringBoot/Logica/ElementoTestNA.java
@@ -2,7 +2,6 @@ package com.Proyecto.SpringBoot.Logica;
 import org.junit.jupiter.api.Test;
 import com.Proyecto.SpringBoot.Logica.*;
 import com.Proyecto.SpringBoot.Logica.Elemento;
-import com.Proyecto.SpringBoot.Modelos.Jugador;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 
